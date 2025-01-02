8ba0aad7127b95a6cd5460d46b2c0a533b482c48
Cyber MacGeddon <cybermaggedon@gmail.com>
2025-01-02 11:16:00 +0000

    Fix lints

--- src/workbench/Workbench.tsx
+++ src/workbench/Workbench.tsx
@@ -1,7 +1,7 @@
 
 import React from 'react';
 
-import { Box, CircularProgress, Typography } from '@mui/material';
+import { Box, Typography } from '@mui/material';
 
 import Banner from './Banner.tsx';
 import Workspace from './Workspace.tsx';
