5a2d20c0533a514a808c582306ee3eddc337f1d4
cybermaggedon <cybermaggedon@gmail.com>
2025-07-15 11:34:03 +0100

    Feature/markdown formatting (#40)
    * Added markdown formatting

--- src/components/chat/ChatMessage.tsx
+++ src/components/chat/ChatMessage.tsx
@@ -1,4 +1,5 @@
 import { Box, Flex, Text, Avatar } from "@chakra-ui/react";
+import Markdown from 'react-markdown-it';
 
 const ChatMessage = ({ message }) => {
   const isUser = message.role === "human";
@@ -19,7 +20,9 @@ const ChatMessage = ({ message }) => {
         px={4}
         py={2}
       >
-        <Text>{message.text}</Text>
+        <Markdown>
+          {message.text}
+        </Markdown>
       </Box>
 
       {isUser && (
