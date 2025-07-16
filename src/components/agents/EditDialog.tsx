44dc8f0f69f4e567a48a77fd5cb6dd779e9c794e
Cyber MacGeddon <cybermaggedon@gmail.com>
2025-07-16 22:41:23 +0100

    Arguments only for prompt

--- src/components/agents/EditDialog.tsx
+++ src/components/agents/EditDialog.tsx
@@ -231,104 +231,108 @@ const EditDialog = ({ open, onOpenChange, onComplete, id, create }) => {
                 />
               )}
 
-              <Table.Root interactive size="xs">
-                <Table.Header>
-                  <Table.Row>
-                    <Table.ColumnHeader>Name</Table.ColumnHeader>
-                    <Table.ColumnHeader>Description</Table.ColumnHeader>
-                    <Table.ColumnHeader>Type</Table.ColumnHeader>
-                  </Table.Row>
-                </Table.Header>
-                <Table.Body>
-                  {args.map((arg, ix) => (
-                    <Table.Row key={ix}>
-                      <Table.Cell width="20%">
-                        <Editable.Root
-                          autoResize={false}
-                          value={arg.name}
-                          onValueChange={(v) =>
-                            setArgAttr(ix, "name", v.value)
-                          }
-                        >
-                          <Editable.Preview />
-                          <Editable.Input />
-                        </Editable.Root>
-                      </Table.Cell>
-                      <Table.Cell width="50%">
-                        <Editable.Root
-                          value={arg.description}
-                          onValueChange={(v) =>
-                            setArgAttr(ix, "description", v.value)
-                          }
-                        >
-                          <Editable.Preview />
-                          <Editable.Input />
-                        </Editable.Root>
-                      </Table.Cell>
-                      <Table.Cell
-                        onClick={() => {
-                          setEditArgIx(ix);
-                        }}
-                      >
-                        {editArgIx == ix && (
-                          <Popover.Root
-                            open={editArgIx == ix}
-                            onOpenChange={(e) => {
-                              if (e) setEditArgIx(-1);
+              {type === "prompt" && (
+                <>
+                  <Table.Root interactive size="xs">
+                    <Table.Header>
+                      <Table.Row>
+                        <Table.ColumnHeader>Name</Table.ColumnHeader>
+                        <Table.ColumnHeader>Description</Table.ColumnHeader>
+                        <Table.ColumnHeader>Type</Table.ColumnHeader>
+                      </Table.Row>
+                    </Table.Header>
+                    <Table.Body>
+                      {args.map((arg, ix) => (
+                        <Table.Row key={ix}>
+                          <Table.Cell width="20%">
+                            <Editable.Root
+                              autoResize={false}
+                              value={arg.name}
+                              onValueChange={(v) =>
+                                setArgAttr(ix, "name", v.value)
+                              }
+                            >
+                              <Editable.Preview />
+                              <Editable.Input />
+                            </Editable.Root>
+                          </Table.Cell>
+                          <Table.Cell width="50%">
+                            <Editable.Root
+                              value={arg.description}
+                              onValueChange={(v) =>
+                                setArgAttr(ix, "description", v.value)
+                              }
+                            >
+                              <Editable.Preview />
+                              <Editable.Input />
+                            </Editable.Root>
+                          </Table.Cell>
+                          <Table.Cell
+                            onClick={() => {
+                              setEditArgIx(ix);
                             }}
                           >
-                            <Popover.Trigger asChild>
-                              <Text>{arg.type}</Text>
-                            </Popover.Trigger>
-                            <Popover.Positioner>
-                              <Popover.Content>
-                                <Popover.Arrow />
-                                <Popover.Body>
-                                  <RadioGroup.Root
-                                    value={args[ix].type}
-                                    onValueChange={(v) =>
-                                      setArgAttr(ix, "type", v.value)
-                                    }
-                                  >
-                                    <Stack gap="6">
-                                      <RadioGroup.Item value={"string"}>
-                                        <RadioGroup.ItemHiddenInput />
-                                        <RadioGroup.ItemIndicator />
-                                        <RadioGroup.ItemText>
-                                          string
-                                        </RadioGroup.ItemText>
-                                      </RadioGroup.Item>
-                                      <RadioGroup.Item value={"number"}>
-                                        <RadioGroup.ItemHiddenInput />
-                                        <RadioGroup.ItemIndicator />
-                                        <RadioGroup.ItemText>
-                                          number
-                                        </RadioGroup.ItemText>
-                                      </RadioGroup.Item>
-                                    </Stack>
-                                  </RadioGroup.Root>
-                                </Popover.Body>
-                              </Popover.Content>
-                            </Popover.Positioner>
-                          </Popover.Root>
-                        )}
-                        {editArgIx != ix && arg.type}
-                      </Table.Cell>
-                    </Table.Row>
-                  ))}
-                </Table.Body>
-              </Table.Root>
+                            {editArgIx == ix && (
+                              <Popover.Root
+                                open={editArgIx == ix}
+                                onOpenChange={(e) => {
+                                  if (e) setEditArgIx(-1);
+                                }}
+                              >
+                                <Popover.Trigger asChild>
+                                  <Text>{arg.type}</Text>
+                                </Popover.Trigger>
+                                <Popover.Positioner>
+                                  <Popover.Content>
+                                    <Popover.Arrow />
+                                    <Popover.Body>
+                                      <RadioGroup.Root
+                                        value={args[ix].type}
+                                        onValueChange={(v) =>
+                                          setArgAttr(ix, "type", v.value)
+                                        }
+                                      >
+                                        <Stack gap="6">
+                                          <RadioGroup.Item value={"string"}>
+                                            <RadioGroup.ItemHiddenInput />
+                                            <RadioGroup.ItemIndicator />
+                                            <RadioGroup.ItemText>
+                                              string
+                                            </RadioGroup.ItemText>
+                                          </RadioGroup.Item>
+                                          <RadioGroup.Item value={"number"}>
+                                            <RadioGroup.ItemHiddenInput />
+                                            <RadioGroup.ItemIndicator />
+                                            <RadioGroup.ItemText>
+                                              number
+                                            </RadioGroup.ItemText>
+                                          </RadioGroup.Item>
+                                        </Stack>
+                                      </RadioGroup.Root>
+                                    </Popover.Body>
+                                  </Popover.Content>
+                                </Popover.Positioner>
+                              </Popover.Root>
+                            )}
+                            {editArgIx != ix && arg.type}
+                          </Table.Cell>
+                        </Table.Row>
+                      ))}
+                    </Table.Body>
+                  </Table.Root>
 
-              <Box mt={5}>
-                <Button
-                  variant="solid"
-                  onClick={() => addArgument()}
-                  colorPalette="brand"
-                  size="xs"
-                >
-                  <Plus /> add argument
-                </Button>
-              </Box>
+                  <Box mt={5}>
+                    <Button
+                      variant="solid"
+                      onClick={() => addArgument()}
+                      colorPalette="brand"
+                      size="xs"
+                    >
+                      <Plus /> add argument
+                    </Button>
+                  </Box>
+                </>
+              )}
             </Dialog.Body>
             <Dialog.Footer>
               <Button variant="outline" onClick={() => onOpenChange(false)}>
