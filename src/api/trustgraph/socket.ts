4f5b8ce685199c37d5992ef5e949a67ea88ff3ab
Cyber MacGeddon <cybermaggedon@gmail.com>
2025-08-20 11:38:08 +0100

    Enable settings in socket provider

--- src/api/trustgraph/socket.ts
+++ src/api/trustgraph/socket.ts
@@ -1,9 +1,15 @@
 import { useContext } from "react";
+import { useSettings } from "../../state/settings";
 
 import { SocketContext } from "./SocketContext";
 
 export const useSocket = () => {
+
   const socket = useContext(SocketContext);
+
+    const { settings } = useSettings();
+
+    console.log(settings);
 
   console.log("useSocket");
   return socket;
